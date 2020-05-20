1e04ae1ea02746dda4f0d65a133bea90ae9a1c2c
aman-kaur11 <63080661+aman-kaur11@users.noreply.github.com>
2020-05-19 21:54:51 -0700

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -66,4 +66,32 @@ void inventory::printItems() {
 	}
 }
 
+void inventory::heapify (int n, int i){
+    int largest = i; // start index as largest
+    int l = 2*i + 1; // left child
+    int r = 2*i + 2; // right child
+    
+    if (l < n && db[l].shelfLife > db[largest].shelfLife)// If left child is greater than root
+        largest = l;
+    
+    if (r < n && db[r].shelfLife > db[largest].shelfLife)// If right child is greater than largest
+        largest = r;
+    
+    if (largest != i)// If largest is not at root
+    {
+        swap(db[i], db[largest]); //swap the largest with the root
+        heapify(n, largest);// Recursive call
+    }
+}
+
+void inventory::heapsort(){
+    for (int i = (numItem/2) - 1; i >= 0; i--)// Build heap (rearrange array)
+        heapify(numItem, i);
+
+    for (int i=numItem-1; i>0; i--)//get elements from heap
+    {
+        swap(db[0], db[i]);// Move current root to end
+        heapify(i, 0);//heapify current heap sort
+    }  
+}
 
